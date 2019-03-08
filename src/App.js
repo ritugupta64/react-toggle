614fc8e475b8ef4604226fc748efefa6ccb3f459
Ritu Gupta <ritugupta@ritugupta-macbookpro.roam.corp.google.com>
2019-03-08 09:12:42 +0530

    one change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+    state = {
+      toggle:false
+    }
+
+    onToggle = () =>{
+      this.setState({
+        toggle:!this.state.toggle
+      })
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <h3>Toggle the any element</h3>
+            <span style={{color:"red", border:"1px solid green",fontSize:"20px",display:"inline-block",padding:"5px"}} onClick = {this.onToggle}>Toggle</span>
+            <br/>
+          <div style={{display:this.state.toggle?"block":"none"}}>
+            Toggle Content
+          </div>
+
+
       </div>
     );
   }
